9ddb9898c55dfb986603de3466848fcaca8a5d3e
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:29:31 -0500

    Update Set-Default-State-for-Checkout.php

--- WooCommerce-Functions/Set-Default-State-for-Checkout/Set-Default-State-for-Checkout.php
+++ WooCommerce-Functions/Set-Default-State-for-Checkout/Set-Default-State-for-Checkout.php
@@ -1,3 +1,5 @@
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // set default state for checkout as FL
 function change_default_checkout_state() {
   return 'FL'; // state code
