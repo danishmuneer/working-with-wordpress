8a861b008806b778b6a328f72ef8b36411a8c68d
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:36:43 -0500

    Update Add-CSS-Only-to-Post-Type-Products.php

--- WooCommerce-Functions/Add-CSS-Only-to-Post-Type-Products/Add-CSS-Only-to-Post-Type-Products.php
+++ WooCommerce-Functions/Add-CSS-Only-to-Post-Type-Products/Add-CSS-Only-to-Post-Type-Products.php
@@ -1,5 +1,5 @@
-
-
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // Add custom CSS to post_type = product
 function product_page_styles() {
 global $post;
