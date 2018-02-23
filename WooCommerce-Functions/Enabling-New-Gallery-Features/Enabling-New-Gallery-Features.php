ec5b1a7a3f5fad919894b504b4097bb8e88d8103
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:10:25 -0500

    Update Enabling-New-Gallery-Features.php

--- WooCommerce-Functions/Enabling-New-Gallery-Features/Enabling-New-Gallery-Features.php
+++ WooCommerce-Functions/Enabling-New-Gallery-Features/Enabling-New-Gallery-Features.php
@@ -1,3 +1,5 @@
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // Add support for gallery custom look
 add_theme_support( 'wc-product-gallery-zoom' );
 add_theme_support( 'wc-product-gallery-lightbox' );
