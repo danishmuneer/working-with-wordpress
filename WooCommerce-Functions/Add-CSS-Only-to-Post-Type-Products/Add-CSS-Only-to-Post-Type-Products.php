a4279e64bfb1770b258c4af77fec47bb7b0a32fb
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:38:08 -0500

    Update Add-CSS-Only-to-Post-Type-Products.php

--- WooCommerce-Functions/Add-CSS-Only-to-Post-Type-Products/Add-CSS-Only-to-Post-Type-Products.php
+++ WooCommerce-Functions/Add-CSS-Only-to-Post-Type-Products/Add-CSS-Only-to-Post-Type-Products.php
@@ -5,7 +5,7 @@ function product_page_styles() {
 global $post;
 if (isset($post)&&$post->post_type=='product') { ?>
    <style type="text/css">
-   .gform_footer.top_label{display: none;}
+   .gform_footer.top_label{display: none;} // This is where you can put any CSS you want to execute only for post type 'products'
    </style>
 <?php } 
 }
