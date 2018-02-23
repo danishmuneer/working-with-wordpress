850907b97dc52c085f213607b0acd646c157edd2
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:22:47 -0500

    Update Set-Default-Country-for-Checkout.php

--- WooCommerce-Functions/Set-Default-Country-for-Checkout/Set-Default-Country-for-Checkout.php
+++ WooCommerce-Functions/Set-Default-Country-for-Checkout/Set-Default-Country-for-Checkout.php
@@ -1,3 +1,5 @@
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // set default country for checkout as US
 function change_default_checkout_country() {
   return 'US'; // country code
