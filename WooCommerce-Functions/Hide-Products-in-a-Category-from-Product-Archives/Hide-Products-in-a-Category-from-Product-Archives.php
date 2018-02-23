8af37130e0b62495f1fbcfa0a77711f97d441e72
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:30:49 -0500

    Create Hide-Products-in-a-Category-from-Product-Archives.php

---
+++ WooCommerce-Functions/Hide-Products-in-a-Category-from-Product-Archives/Hide-Products-in-a-Category-from-Product-Archives.php
@@ -0,0 +1,17 @@
+/* Hides products in specific category from the SHOP page */
+function custom_pre_get_posts_query( $q ) {
+
+    $tax_query = (array) $q->get( 'tax_query' );
+
+    $tax_query[] = array(
+           'taxonomy' => 'product_cat',
+           'field' => 'slug',
+           'terms' => array( 'industrial-products' ), // Don't display products in the industrial-products category on the shop page.
+           'operator' => 'NOT IN'
+    );
+
+
+    $q->set( 'tax_query', $tax_query );
+
+}
+add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' );
