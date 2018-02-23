9d1c32587f139b15a7c91d293b0d183c3d388899
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:38:56 -0500

    Update add-css-class-to-body.php

--- Genesis-Theme-Functions/Add-CSS-Class-to-Body/add-css-class-to-body.php
+++ Genesis-Theme-Functions/Add-CSS-Class-to-Body/add-css-class-to-body.php
@@ -2,5 +2,5 @@
 /*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // Add specific CSS class by filter
 add_filter( 'body_class', function( $classes ) {
-    return array_merge( $classes, array( 'custom-css-class-1','custom-css-class-2' ) );
+    return array_merge( $classes, array( 'custom-css-class-1','custom-css-class-2' ) ); // The array can contain an unlimited number of post IDs
 } );
