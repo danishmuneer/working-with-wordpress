abb099d4a88dd926785cc39410bca86d2219bf76
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:18:14 -0500

    Update Display-12-Products-on-Product-Archive-Page.php

--- WooCommerce-Functions/Display-12-Products-on-Product-Archive-Page/Display-12-Products-on-Product-Archive-Page.php
+++ WooCommerce-Functions/Display-12-Products-on-Product-Archive-Page/Display-12-Products-on-Product-Archive-Page.php
@@ -1,2 +1,4 @@
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // Display 12 products per page
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 12;' ), 20 );
