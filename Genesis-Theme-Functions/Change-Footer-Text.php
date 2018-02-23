c9171fca29b6e85dd92ee828b5ace62ccaf7c580
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 19:59:03 -0500

    Create Change-Footer-Text.php
    This piece of code removes the existing footer text from a WordPress site using Genesis child theme.

---
+++ Genesis-Theme-Functions/Change-Footer-Text.php
@@ -0,0 +1,9 @@
+<?php
+//* Customize the entire footer
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'dan_custom_footer' );
+function dan_custom_footer() {
+	?>
+	<p>&copy; Copyright 1990-2018 &middot; Danish Muneer</p>
+	<?php
+}
