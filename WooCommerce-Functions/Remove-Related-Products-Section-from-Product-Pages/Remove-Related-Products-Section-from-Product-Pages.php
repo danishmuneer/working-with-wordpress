48a00fe98c01937c88c9ab5beb9ec56796ee0355
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:22:10 -0500

    Update Remove-Related-Products-Section-from-Product-Pages.php

--- WooCommerce-Functions/Remove-Related-Products-Section-from-Product-Pages/Remove-Related-Products-Section-from-Product-Pages.php
+++ WooCommerce-Functions/Remove-Related-Products-Section-from-Product-Pages/Remove-Related-Products-Section-from-Product-Pages.php
@@ -1,2 +1,4 @@
+<?php 
+/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
 // remove related product for woocommerce 3.0.5
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
