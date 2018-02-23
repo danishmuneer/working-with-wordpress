82528ab24d9cccce6c19a164407109d50d087d45
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:13:56 -0500

    Create Dequeue-WC-Cart-Gragment.php

---
+++ WooCommerce-Functions/Dequeue-WC-Cart-Fragment/Dequeue-WC-Cart-Gragment.php
@@ -0,0 +1,6 @@
+// DeQueue the WC cart fragment resource to decrease page load time
+function de_script() {
+    wp_dequeue_script( 'wc-cart-fragments' );
+    return true;
+}
+add_action( 'wp_print_scripts', 'de_script', 100 );
