99ebebe83dc8c7134fec4e99e95e6db0df39ec64
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 20:28:33 -0500

    Create Set-Default-State-for-Checkout.php

---
+++ WooCommerce-Functions/Set-Default-State-for-Checkout/Set-Default-State-for-Checkout.php
@@ -0,0 +1,5 @@
+// set default state for checkout as FL
+function change_default_checkout_state() {
+  return 'FL'; // state code
+}
+add_filter( 'default_checkout_billing_state', 'change_default_checkout_state' );
