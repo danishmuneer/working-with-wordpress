e07ede760e8799a2a23a739a09a2046819e9ebb4
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:02:07 -0500

    Update Add-Default_Quantity-to-a-Product-Field.php

--- Gravity-Forms-Functions/Add-Default_Quantity-to-a-Product-Field/Add-Default_Quantity-to-a-Product-Field.php
+++ Gravity-Forms-Functions/Add-Default_Quantity-to-a-Product-Field/Add-Default_Quantity-to-a-Product-Field.php
@@ -1,7 +1,7 @@
 <?php 
 /*^^^ Remove this php tag before adding this code to your theme's functions.php file */
 // Configure quantity input value be '1'
-add_filter( 'gform_field_value_default_quantity', 'endo_set_default_quantity' );
-function endo_set_default_quantity() {
+add_filter( 'gform_field_value_default_quantity', 'dan_set_default_quantity' );
+function dan_set_default_quantity() {
     return 1; // change this number to whatever you want the default quantity to be
 }
