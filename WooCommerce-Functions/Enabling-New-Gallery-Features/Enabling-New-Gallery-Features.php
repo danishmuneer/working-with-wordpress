609c8c9d0d19c602781bd7185fa91249b6fb788e
Danish Muneer <danish.muneer@outlook.com>
2018-02-22 21:09:55 -0500

    Create Enabling-New-Gallery-Features.php

---
+++ WooCommerce-Functions/Enabling-New-Gallery-Features/Enabling-New-Gallery-Features.php
@@ -0,0 +1,4 @@
+// Add support for gallery custom look
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
