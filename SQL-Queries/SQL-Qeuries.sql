89475983b5c0703d637d8f3a28fb216f47b15917
Danish Muneer <danish.muneer@outlook.com>
2020-04-13 13:46:27 -0400

    Update SQL-Qeuries.sql

--- SQL-Queries/SQL-Qeuries.sql
+++ SQL-Queries/SQL-Qeuries.sql
@@ -2,3 +2,12 @@
 to just check the query. Replace "SELECT *" with DELETE to delete the rows */
 
 SELECT * FROM `wp_postmeta` WHERE `post_id` IN ( SELECT ID FROM `wp_posts` ) LIMIT 50
+
+/* the following query builds upon the previous by finding the common data using INNER JOIN */
+
+SELECT      `wp_postmeta`.`post_id`,`wp_postmeta`.`meta_key`,`wp_postmeta`.`meta_value`
+FROM        `wp_postmeta`
+INNER JOIN  `wp_posts` ON `wp_postmeta`.`post_id` = `wp_posts`.`ID`
+WHERE       `wp_postmeta`.`meta_key` = "_order_total" OR `wp_postmeta`.`meta_key` = "_paid_date" 
+ORDER BY	`wp_postmeta`.`post_id`	
+LIMIT       50
