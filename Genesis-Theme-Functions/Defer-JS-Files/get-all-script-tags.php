6d445911706047fa0bdca6a83fc32449457406ec
Danish Muneer <danish.muneer@outlook.com>
2020-06-29 14:43:47 -0400

    Create get-all-script-tags.php

---
+++ Genesis-Theme-Functions/Defer-JS-Files/get-all-script-tags.php
@@ -0,0 +1,10 @@
+function detect_enqueued_scripts() {
+  global $wp_scripts;
+  echo "<div style='display:none;'>Handles: ";
+  foreach( $wp_scripts->queue as $handle ) {
+   echo $handle . ', ';
+  }
+  echo "</div>";
+}
+
+add_action( 'wp_print_scripts', 'detect_enqueued_scripts' );
