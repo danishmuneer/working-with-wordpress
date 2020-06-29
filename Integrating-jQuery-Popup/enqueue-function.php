025ab5e02471898e938879aa5c686c94881f94fe
Danish Muneer <danish.muneer@outlook.com>
2020-06-29 13:30:03 -0400

    Update enqueue-function.php

--- Integrating-jQuery-Popup/enqueue-function.php
+++ Integrating-jQuery-Popup/enqueue-function.php
@@ -1,5 +1,6 @@
-// This code goes in the functions.php - enqueues the JS file in the footer
+<?php
 
+// This code goes in the functions.php - enqueues the JS file in the footer
 function custom_popuptips() {
 	wp_enqueue_script('tip-popups', get_stylesheet_directory_uri(). "/tip-popups.js", array('jquery') ,'1.0', true );
 }
