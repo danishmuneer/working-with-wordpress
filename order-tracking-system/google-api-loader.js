8c9132aeb9c1d119cd588511486e26da10a6155a
Danish Muneer <danish.muneer@outlook.com>
2020-06-29 13:28:06 -0400

    Create google-api-loader.js

---
+++ order-tracking-system/google-api-loader.js
@@ -0,0 +1,6 @@
+<!---google api loader - add this to end of footer.php --->
+<script async defer src="https://apis.google.com/js/api.js"
+	onload="this.onload=function(){};handleClientLoad()"
+	onreadystatechange="if (this.readyState === 'complete') this.onload()">
+</script>
+
