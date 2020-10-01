7734a978a710278caa31d8e2c0b0fe2ef0c12cd8
Danish Muneer <danish.muneer@outlook.com>
2020-10-01 13:47:08 -0400

    Update SQL-Qeuries.sql

--- SQL-Queries/SQL-Qeuries.sql
+++ SQL-Queries/SQL-Qeuries.sql
@@ -11,3 +11,14 @@ INNER JOIN  `wp_posts` ON `wp_postmeta`.`post_id` = `wp_posts`.`ID`
 WHERE       `wp_postmeta`.`meta_key` = "_order_total" OR `wp_postmeta`.`meta_key` = "_paid_date" 
 ORDER BY	`wp_postmeta`.`post_id`	
 LIMIT       50
+
+/* select all gravity form entries uptill a certain date */
+
+SELECT * FROM `wp_gf_entry` WHERE `date_created` < 'YYYY-MM-DD'  
+ORDER BY `wp_gf_entry`.`date_created` DESC
+
+/* select all gravity form entries metas uptill the highest entry ID in the date range returned by above query */
+SELECT * FROM `wp_gf_entry_meta`
+WHERE `entry_id` IN ( SELECT ID FROM `wp_gf_entry` )
+AND `entry_id` <= xxx /* replace xxx by the highest entry ID number we got from the previous query*/
+ORDER BY `wp_gf_entry_meta`.`entry_id`  DESC
