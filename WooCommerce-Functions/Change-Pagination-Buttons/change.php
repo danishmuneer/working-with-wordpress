bf4038813520638e7bf6fc48f37f5f78a42e1f60
Daniteq <support@daniteq.com>
2022-04-24 02:58:43 -0400

    Delete WooCommerce-Functions/Change-Pagination-Buttons directory

--- WooCommerce-Functions/Change-Pagination-Buttons/change.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* change the next and previous buttons' text on WC pagination */
-function custom_woo_pagination( $args ) {
-	$args['prev_text'] = '<i class="fa fa-angle-left"></i>';
-	$args['next_text'] = '<i class="fa fa-angle-right"></i>';
-	return $args;
-}
-add_filter( 'woocommerce_pagination_args', 	'custom_woo_pagination' );
--- WooCommerce-Functions/Change-Pagination-Buttons/readme.txt
+++
@@ -1,1 +0,0 @@
-Change pagination buttons on archive pages. Make sure font awesome is installed on the website
