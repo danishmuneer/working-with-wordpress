a4e867f2b95828853b84e6bc60ba4a0971309fa3
Daniteq <support@daniteq.com>
2022-04-24 23:06:20 -0400

    Delete WooCommerce-Functions/Enabling-New-Gallery-Features directory

--- WooCommerce-Functions/Enabling-New-Gallery-Features/Enabling-New-Gallery-Features.php
+++
@@ -1,6 +0,0 @@
-<?php 
-/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
-// Add support for gallery custom look
-add_theme_support( 'wc-product-gallery-zoom' );
-add_theme_support( 'wc-product-gallery-lightbox' );
-add_theme_support( 'wc-product-gallery-slider' );
