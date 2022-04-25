f5723ec194f46821ef4d74ebed8a720df9e37282
Daniteq <support@daniteq.com>
2022-04-24 23:51:30 -0400

    Delete WooCommerce-Functions/Dequeue-WC-Cart-Fragment directory

--- WooCommerce-Functions/Dequeue-WC-Cart-Fragment/Dequeue-WC-Cart-Fragment.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/*^^^ You might have to remove this php tag before adding this code to your theme's functions.php file */
-// DeQueue the WC cart fragment resource to decrease page load time
-function de_script() {
-    wp_dequeue_script( 'wc-cart-fragments' );
-    return true;
-}
-add_action( 'wp_print_scripts', 'de_script', 100 );
